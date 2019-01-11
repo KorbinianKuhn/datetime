e6eb747ab4d008f7641f4c1649dd7ead0b14595e
marcSche <marcSche@users.noreply.github.com>
2019-01-11 20:06:10 +0100

    added tests (#5)
    * added test cases for diff() and fixed diff() function
    
    * added tests

--- test/isSameOrBefore.spec.js
+++ test/isSameOrBefore.spec.js
@@ -4,8 +4,41 @@ const datetime = require('../dist');
 describe('isSameOrBefore()', () => {
   const dates = [
     new Date('2017-01-01T00:00:00.000Z'),
+    new Date('2017-05-26T21:21:34.381Z'),
     new Date('2018-01-01T00:00:00.000Z'),
-    new Date('2019-01-01T00:00:00.000Z')
+    new Date('2018-01-01T23:00:00.000Z'),
+    new Date('2019-01-01T00:00:00.000Z'),
+    new Date('2019-12-31T23:59:59.999Z'),
+    new Date('2019-12-31T00:00:00.000Z'),
+    new Date('2020-11-30T23:59:59.999Z'),
+    new Date('2020-11-30T00:00:00.000Z'),
+    new Date('2021-04-30T23:59:59.999Z'),
+    new Date('2021-04-01T23:59:59.999Z'),
+    new Date('2021-04-01T00:00:00.000Z'),
+    new Date('2020-03-23T00:00:00.000Z'),
+    new Date('2020-03-31T00:00:00.000Z'),
+    new Date('2020-10-23T00:00:00.000Z'),
+    new Date('2020-10-31T00:00:00.000Z'),
+    new Date('2021-03-23T00:00:00.000Z'),
+    new Date('2021-03-31T00:00:00.000Z'),
+    new Date('2021-10-23T00:00:00.000Z'),
+    new Date('2021-10-31T00:00:00.000Z'),
+    new Date('2020-03-23T23:59:59.999Z'),
+    new Date('2020-03-31T23:59:59.999Z'),
+    new Date('2020-10-23T23:59:59.999Z'),
+    new Date('2020-10-31T23:59:59.999Z'),
+    new Date('2021-03-23T23:59:59.999Z'),
+    new Date('2021-03-31T23:59:59.999Z'),
+    new Date('2021-07-23T23:59:59.999Z'),
+    new Date('2021-08-23T23:59:59.999Z'),
+    new Date('2021-07-23T20:59:59.999Z'),
+    new Date('2021-08-23T20:59:59.999Z'),
+    new Date('2021-08-22T20:59:59.999Z'),
+    new Date('2021-08-24T20:59:59.999Z'),
+    new Date('2021-10-23T23:59:59.999Z'),
+    new Date('2021-10-31T23:59:59.999Z'),
+    new Date('2021-09-23T23:59:59.999Z'),
+    new Date('2021-09-30T23:59:59.999Z')
   ];
 
   const units = [
@@ -19,20 +52,20 @@ describe('isSameOrBefore()', () => {
     'year'
   ];
 
-  const dateB = new Date('2018-01-01T00:00:00.000Z');
-
   units.map(unit => {
-    dates.map(date => {
-      it(`with unit ${unit} and date ${date.toISOString()} should verify`, () => {
-        const expected = moment(date).isSameOrBefore(dateB, unit);
-        const actual = datetime(date).isSameOrBefore(dateB, unit);
-        expect(actual).toEqual(expected);
-      });
+    dates.map(date1 => {
+      dates.map(date2 => {
+        it(`with unit ${unit} and date1 ${date1.toISOString()} and date2 ${date2.toISOString()} should verify`, () => {
+          const expected = moment(date1).isSameOrBefore(date2, unit);
+          const actual = datetime(date1).isSameOrBefore(date2, unit);
+          expect(actual).toEqual(expected);
+        });
 
-      it(`utc: with unit ${unit} and date ${date.toISOString()} should verify`, () => {
-        const expected = moment.utc(date).isSameOrBefore(dateB, unit);
-        const actual = datetime.utc(date).isSameOrBefore(dateB, unit);
-        expect(actual).toEqual(expected);
+        it(`utc: with unit ${unit} and date1 ${date1.toISOString()} and date2 ${date2.toISOString()} should verify`, () => {
+          const expected = moment.utc(date1).isSameOrBefore(date2, unit);
+          const actual = datetime.utc(date1).isSameOrBefore(date2, unit);
+          expect(actual).toEqual(expected);
+        });
       });
     });
   });
